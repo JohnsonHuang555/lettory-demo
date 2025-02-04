a2955ea73317d1fa99a1fe16bff6cff4007febf2
Johnson Huang <johnsonhuang@xaregroup.com>
2025-02-04 18:45:51 +0800

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,97 @@
-import Image from "next/image";
+/* eslint-disable @typescript-eslint/no-explicit-any */
+'use client';
+
+import { useEffect, useState } from "react";
+
+const countOccurrences = (arr: number[]) => {
+  const result: any = {};
+  arr.forEach(num => {
+    result[num] = (result[num] || 0) + 1;
+  });
+  return result;
+};
+
+// 取得出現次數最多的前三個數字
+// const getTopThree = (occurrences: any) => {
+//   if (occurrences) {
+//     // 將物件轉換為陣列，並根據出現次數排序
+//     const sortedOccurrences = Object.entries(occurrences).sort((a: any, b: any) => b[1] - a[1]);
+
+//     // 取前三名
+//     return sortedOccurrences.slice(0, 3).map(item => item[0]);
+//   }
+// };
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [totalNumberCount, setTotalNumberCount] = useState<any>();
+  const [defaultSize, setDefaultSize] = useState(5)
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  // const topThree = getTopThree(totalNumberCount);
+
+  useEffect(() => {
+    const fetchNewNumbers = async () => {
+      const res = await fetch(`https://api.taiwanlottery.com/TLCAPIWeB/Lottery/BingoResult?openDate=2025-02-04&pageNum=1&pageSize=${defaultSize}`)
+      const data = await res.json();
+      const newData = data.content.bingoQueryResult.reduce((acc: any, current: any) => {
+        acc.push(current.bigShowOrder);
+        return acc;
+      }, []).flat();
+
+      const occurrences = countOccurrences(newData.map((a: any) => Number(a)));
+      setTotalNumberCount(occurrences)
+    }
+
+    fetchNewNumbers();
+  }, [defaultSize])
+
+  const getBgColor = (count: number) => {
+    switch (count) {
+      case 0:
+        return '';
+      case 1:
+        return 'bg-red-100';
+      case 2:
+        return 'bg-red-200';
+      case 3:
+        return 'bg-red-300';
+      case 4:
+        return 'bg-red-400 text-white';
+      case 5:
+        return 'bg-red-500 text-white';
+      case 6:
+        return 'bg-red-600 text-white';
+      case 7:
+        return 'bg-red-700 text-white';
+      default:
+        return 'bg-red-900 text-white';
+    }
+  }
+
+  return (
+    <div className="p-12">
+      <main className="flex flex-col justify-center items-center">
+        {/* <h2 className="mb-4">期數</h2>
+        <div className="flex gap-4 mb-8">
+          {topThree?.map(t => (<div key={t} className="text-lg border rounded-md px-4 py-2 flex items-center justify-center">{t}</div>))}
+        </div> */}
+        <div className="flex mb-8">
+          <div>期數</div>
+          <select className="ml-2 w-[100px] border" defaultValue={defaultSize} onChange={(e) => setDefaultSize(Number(e.target.value))}>
+            <option value={3}>3</option>
+            <option value={5}>5</option>
+            <option value={10}>10</option>
+            <option value={20}>20</option>
+          </select>
         </div>
+        {totalNumberCount ? <div className="w-[1000px] grid grid-cols-8 gap-3">
+          {Array.from({ length: 80 }).map((_, index) => (
+            <div key={index} className={`flex-col h-20 border text-xl justify-center items-center flex rounded-md ${getBgColor(totalNumberCount[index + 1] || 0)}`}>
+              <div className="text-[26px] mb-1">{index + 1}</div>
+              <div className="text-base">次數：{totalNumberCount[index + 1] ? totalNumberCount[index + 1] : 0}</div>
+            </div>
+          ))}
+        </div>: null}
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
